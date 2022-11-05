7400bac51d30691deaa9ea318c84031685133a45
ItRealRew <rew-proninmax@yandex.ru>
2022-11-05 15:51:15 +0300

    Refactoring and Fix Bug

---
+++ TXSTBXRD/TXSTBXRD-UI/Utils/Libs/RegularStorageName.cs
@@ -0,0 +1,8 @@
+namespace TXSTBXRD_UI.Utils.Libs
+{
+    public class RegularStorageName
+    {
+        internal const string AuthorizationToken = "authorizationToken";
+        internal const string RememberedUserName = "rememberedUserName";
+    }
+}
