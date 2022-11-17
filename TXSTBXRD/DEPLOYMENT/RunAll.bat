72a2eefa777392c3970396690cc68c00fef68fd5
ItRealRew <rew-proninmax@yandex.ru>
2022-11-17 17:40:05 +0300

    Start IMPL Communications Service

--- TXSTBXRD/DEPLOYMENT/RunAll.bat
+++ TXSTBXRD/DEPLOYMENT/RunAll.bat
@@ -2,7 +2,7 @@ cd ..
 
 dotnet build
 
-start /d "."  dotnet run --project .\TXSTBXRD-MIDDLEWARE\TXSTBXRD-MIDDLEWARE.csproj
 start /d "."  dotnet run --project .\TXTBXRD-SERVICES\IDENTITY-SERVICE\IDENTITY-SERVICE.csproj
+start /d "."  dotnet run --project .\TXTBXRD-SERVICES\IDENTITY-SERVICE\COMMUNICATIONS-SERVICE.csproj
 start /d "."  dotnet run --project .\TXTBXRD-SERVICES\EVENT-SERVICE\EVENT-SERVICE.csproj
 start /d "."  dotnet run --project .\TXSTBXRD-UI\TXSTBXRD-UI.csproj
---
+++ TXSTBXRD/TXTBXRD-SERVICES/COMMUNICATIONS-SERVICE/Controllers/CommunicationsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMMUNICATIONS_SERVICE.Controllers
+{
+    [Route("[controller]")]
+    public class CommunicationsController : ControllerBase
+    {
+        
+    }
+}
