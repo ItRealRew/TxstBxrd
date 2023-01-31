99015b4c771fb4b9146321512b6e4158a60e67a6
ItRealRew <rew-proninmax@yandex.ru>
2023-01-31 16:55:04 +0300

    Added Notification Component

--- TXSTBXRD/TXSTBXRD-UI/Utils/HttpInterceptorService.cs
+++ TXSTBXRD/TXSTBXRD-UI/Utils/HttpInterceptorService.cs
@@ -7,15 +7,18 @@ namespace TXSTBXRD_UI.Utils
     {
         private readonly HttpClientInterceptor _interceptor;
         private readonly NavigationManager _navManager;
-        public HttpInterceptorService(HttpClientInterceptor interceptor, NavigationManager navManager)
+        private readonly NotificationService _notification;
+        public HttpInterceptorService(HttpClientInterceptor interceptor, NavigationManager navManager, NotificationService notification)
         {
             _interceptor = interceptor;
             _navManager = navManager;
+            _notification = notification;
         }
         public void RegisterEvent() => _interceptor.AfterSend += InterceptResponse;
         private void InterceptResponse(object sender, HttpClientInterceptorEventArgs e)
         {
             //_navManager.NavigateTo("/404");
+            _notification.ShowToast("FFFFFF", Utils.Types.NotificationLevel.Error);
         }
         public void DisposeEvent() => _interceptor.AfterSend -= InterceptResponse;
     }
